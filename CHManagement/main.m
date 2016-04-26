007fa80974c89c3c73cda4f7497d5b64b3e21e8e
Troy Peng <troy.peng@sap.com>
2016-04-26 21:58:30 +0800

    Initial Commit

---
+++ CHManagement/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CHManagement
+//
+//  Created by Peng, Troy on 4/26/16.
+//  Copyright © 2016 楚淮集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
