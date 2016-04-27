cda328b265d947f06a91c5b4641c913668f4e1ac
Troy Peng <troy.peng@sap.com>
2016-04-27 11:06:25 +0800

    Add two VO file

---
+++ CHManagement/VO/AccountVO.h
@@ -0,0 +1,15 @@
+//
+//  AccountVO.h
+//  CHManagement
+//
+//  Created by Peng, Troy on 4/27/16.
+//  Copyright © 2016 楚淮集团. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AccountVO : NSObject
+
+@property (copy) NSString* name;
+
+@end
